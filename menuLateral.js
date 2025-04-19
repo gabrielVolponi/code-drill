1ff8ef7b3e80ae678ecb048f1fc143d477f518d5
Vasseli <nicolaszielasko@gmail.com>
2025-04-18 23:50:14 -0300

    Funcionalidade close sidebar

--- menuLateral.js
+++ menuLateral.js
@@ -2,12 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuButton = document.getElementById("menuButton");
   const sidebar = document.getElementById("sidebar");
   const overlay = document.getElementById("overlay");
+  const closeButton = document.getElementById("closeSide");
 
   menuButton.addEventListener("click", (e) => {
     sidebar.classList.remove("translate-x-full");
     overlay.classList.remove("hidden");
   });
 
+  closeButton.addEventListener("click", () => {
+    sidebar.classList.add("translate-x-full");
+    overlay.classList.add("hidden");
+  });
 
   document.addEventListener("click", (e) => {
     const clickedInsideSidebar = sidebar.contains(e.target);
